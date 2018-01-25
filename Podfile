5bd93b57a8c235278cba1f4e3b0a14e87030b07d
WangXueqi <jkwxq@sina.com>
2018-01-25 10:05:29 +0800

    gif

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+  platform :ios, '8.0'
+
+target '制作动图' do
+  # Uncomment the next line if you're using Swift or would like to use dynamic frameworks
+  # use_frameworks!
+
+  # Pods for 制作动图UITests
+pod 'SDWebImage', '~> 4.2.3'
+  target '制作动图Tests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target '制作动图UITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- 制作动图/ViewController.m
+++ 制作动图/ViewController.m
@@ -7,50 +7,74 @@
 //
 
 #import "ViewController.h"
-
+#import <UIImage+GIF.h>
 @interface ViewController ()
-
+@property(nonatomic,strong)UIImageView * gifImage;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-    self.view.backgroundColor = [UIColor grayColor];
-    [self imageGif];
+    self.view.backgroundColor = [UIColor whiteColor];
+//    [self imageGif];
+    [self webViewGif];
 }
 
 //UIImageView实现gif动画
 - (void)imageGif {
 
-    UIImageView * gifImage = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, 150, 150)];
+    UIImageView * gifImage = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, 160, 20)];
     gifImage.center = self.view.center;
     [self.view addSubview:gifImage];
+    self.gifImage = gifImage;
     
-    NSMutableArray * images = [NSMutableArray array];
-    for (int i=1; i < 5; i++) {
-        [images addObject:[UIImage imageNamed:[NSString stringWithFormat:@"sound_%d",i]]];
+    NSInteger A = 1;
+    if (A<0) {
+        //多图片
+        [self creatRightButton];
+        NSMutableArray * images = [NSMutableArray array];
+        for (int i=2; i < 14; i++) {
+            [images addObject:[UIImage imageNamed:[NSString stringWithFormat:@"spot_%d",i]]];
+        }
+        gifImage.animationImages = images;
+        gifImage.animationDuration = 1.0;//完成一次时间
+        gifImage.animationRepeatCount = NSUIntegerMax;//播放次数
+    }else{
+        //gif动图
+        NSString *path = [[NSBundle mainBundle] pathForResource:@"spot" ofType:@"gif"];
+        NSData *data = [NSData dataWithContentsOfFile:path];
+        [self.gifImage setImage:[UIImage sd_animatedGIFWithData:data]];
     }
-    gifImage.animationImages = images;
-    gifImage.animationDuration = 2.0;//完成一次时间
-    gifImage.animationRepeatCount = NSUIntegerMax;//播放次数
-    [gifImage startAnimating];
-    
 }
 
 //UIWebView实现.gif动画文件的播放
 - (void)webViewGif {
 
-    CGSize size = [UIImage imageNamed:@"image.gif"].size;
+    CGSize size = [UIImage imageNamed:@"spot.gif"].size;
     UIWebView * webView = [[UIWebView alloc]initWithFrame:CGRectMake(0, 0, size.width, size.height)];
     webView.center = self.view.center;
     webView.userInteractionEnabled = NO;
     webView.backgroundColor = [UIColor whiteColor];
     [self.view addSubview:webView];
-    NSData * data = [NSData dataWithContentsOfFile:[[NSBundle mainBundle]pathForResource:@"image" ofType:@"gif"]];
+    NSData * data = [NSData dataWithContentsOfFile:[[NSBundle mainBundle]pathForResource:@"spot" ofType:@"gif"]];
     [webView loadData:data MIMEType:@"image/gif" textEncodingName:nil baseURL:nil];
     
+}
+
+- (void)creatRightButton{
+    
+    UIBarButtonItem * rightButton = [[UIBarButtonItem alloc]initWithTitle:@"点击" style:UIBarButtonItemStyleDone target:self action:@selector(rightButtonClick:)];
+    self.navigationItem.rightBarButtonItem = rightButton;
+}
+
+- (void)rightButtonClick:(UIBarButtonItem *)sender {
+    
+    if (![self.gifImage isAnimating]) {
+        [self.gifImage startAnimating];
+    }else{
+        [self.gifImage stopAnimating];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
